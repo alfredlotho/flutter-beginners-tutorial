8238fddbb201546756866fd1ab6878895249c6e6
alfredlotho <alfred.lotho@gmail.com>
2023-12-08 12:30:11 -0800

    add colors and fonts

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!!')
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text(
+          'hello, ninjas!',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.green[100],
+            fontFamily: 'IndieFlower',
+          ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+      onPressed: () {
+      },
+    ),
+  ),
 ));
 
 
